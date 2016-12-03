6bad6504f955ce51d531020178edcdd8899df0b9
vipinbb <captainmohyal@gmail.com>
2016-12-03 12:16:29 -0500

    Added proper data and decoding for inertial data

---
+++ ece1747_finalproj_mapreduce/Decoder.java
@@ -0,0 +1,35 @@
+package com.ece1747.mapreduce;
+
+import java.io.*;
+import java.text.ParseException;
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.PATH;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class Decoder
+{
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
