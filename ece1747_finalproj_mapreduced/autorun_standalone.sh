382b2f78fe639eeb0221a03d09b67b05f417f475
vipinbb <captainmohyal@gmail.com>
2016-12-04 00:53:45 -0500

    standalone cleanup

--- ece1747_finalproj_mapreduced/autorun_standalone.sh
+++ ece1747_finalproj_mapreduced/autorun_standalone.sh
@@ -5,10 +5,12 @@
 # PURPOSE:This script compiles the Java program using Hadoop and binds with all the dependencies. Then it runs the Hadoop application in Pseude-Distributed mode.
 
 # Compile using Hadoop and create JAR.
-echo "Compiling program\n"
+echo "Compiling program..."
 hadoop com.sun.tools.javac.Main App.java
 jar cf App.jar App*.class
 
 # Run in standalone mode.
-echo "Running Hadoop Task"
-hadoop jar App.jar App ../data/*.csv ../data/output
+echo "Running Hadoop Task..."
+rm -r ../data/outputs
+hadoop jar App.jar App ../data/* ../data/outputs/
+
