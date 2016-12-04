18e09c9d2d78bc4566c19a6f496517435b2cd8f3
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-12-04 14:40:46 -0500

    Put actual data file in HDFS and change program parameters

--- scripts/run_pseudo_distributed_docker.sh
+++ scripts/run_pseudo_distributed_docker.sh
@@ -6,7 +6,7 @@ $HADOOP_HOME/sbin/start-yarn.sh
 
 # Create input directory in HDFS (/var/app/hadoop/data) and put a file there
 hadoop fs -mkdir -p /var/app/hadoop/data/input
-hadoop fs -put $HADOOP_HOME/LICENSE.txt /var/app/hadoop/data/input
+hadoop fs -put /home/hadoop/ece1747_final_project/data/acceldata_stationary.csv /var/app/hadoop/data/input
 
 # Run MapReduce job
-hadoop jar app.jar App /var/app/hadoop/data/input/* /var/app/hadoop/data/output
+hadoop jar app.jar App /var/app/hadoop/data/input /var/app/hadoop/data/output
