265bbbf5e47de0b60da1eee9ac4c4a67acd3af57
Naba Sadia Siddiqui <naba.sadia.siddiqui@gmail.com>
2016-12-04 13:37:05 -0500

    Fix path of HDFS

--- scripts/run_pseudo_distributed_docker.sh
+++ scripts/run_pseudo_distributed_docker.sh
@@ -6,7 +6,7 @@ $HADOOP_HOME/sbin/start-yarn.sh
 
 # Create input directory in HDFS (/var/app/hadoop/data) and put a file there
 hadoop fs -mkdir -p /var/app/hadoop/data/input
-hadoop fs -put $HADOOP_HOME/LICENSE.txt /var/app/hadoop/input
+hadoop fs -put $HADOOP_HOME/LICENSE.txt /var/app/hadoop/data/input
 
 # Run MapReduce job
 hadoop jar app.jar App /var/app/hadoop/data/input/* /var/app/hadoop/data/*
